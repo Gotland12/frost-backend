36af41e841fe54e08b6a3097b949be1edf085708
Gotland12 <jockejson@hotmail.com>
2026-06-27 16:19:57 +0200

    Fetch securities from database in API endpoint
    Read securities from database

--- app/main.py
+++ app/main.py
@@ -1,16 +1,33 @@
 from fastapi import FastAPI
 
+from database import SessionLocal
+from models import Security
+from sqlalchemy import text
+
+
 app = FastAPI(title="FROST Backend")
 
 @app.get("/")
 def root():
     return {"status": "ok"}
 
+
 @app.get("/api/v1/securities")
 def securities():
-    return [
-        {"ticker": "NVDA", "name": "NVIDIA"},
-        {"ticker": "MSFT", "name": "Microsoft"},
-        {"ticker": "ASML", "name": "ASML"},
-        {"ticker": "SAP", "name": "SAP"}
-    ]
+    db = SessionLocal()
+
+    result = db.execute(
+        text("SELECT ticker, name FROM securities")
+    )
+
+    securities = []
+
+    for row in result:
+        securities.append({
+            "ticker": row.ticker,
+            "name": row.name
+        })
+
+    db.close()
+
+    return securities
