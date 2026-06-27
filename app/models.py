6f109bee1f8a11e0e0e1c5fe428222d80f651040
Gotland12 <jockejson@hotmail.com>
2026-06-27 09:40:44 +0200

    Refactor Security model to use SQLAlchemy ORM
    Create SQLAlchemy Security model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,14 @@
-class Security:
-    def __init__(self, ticker, name):
-        self.ticker = ticker
-        self.name = name
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Column, Integer, String
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Security(Base):
+    __tablename__ = "securities"
+
+    id = Column(Integer, primary_key=True)
+    ticker = Column(String(20))
+    name = Column(String(100))
